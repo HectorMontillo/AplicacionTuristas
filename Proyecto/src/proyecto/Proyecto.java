54ebf9266d9554562d9089d94e9d59c12bad871c
Hector Montillo <hector.montillo@utp.edu.co>
2018-06-04 19:00:35 -0500

    La creacion de los objetos view ahora se hacen en el controlador y no el en main

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -2,17 +2,16 @@
 package proyecto;
 
 import controlador.Controlador;
-import javax.swing.JFrame;
-import modelo.Modelo;
-import vista.ViewLogin;
+
 
 public class Proyecto {
 
     public static void main(String[] args) {
 
-        JFrame view = new ViewLogin();
-        Modelo modelo = new Modelo();
-        Controlador controlador = new Controlador((ViewLogin) view,modelo);
+        //JFrame view = new ViewLogin();
+        //Modelo modelo = new Modelo();
+        Controlador controlador = new Controlador();
+        controlador.iniciar();
 
     }
     
